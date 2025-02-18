1342732d95aab076ed97c7d581f786bcdfe0a713
张飞 <156419696+MinManchiZ@users.noreply.github.com>
2025-02-19 00:54:31 +0800

    Delete app directory

--- app/__init__.py
+++
@@ -1,8 +0,0 @@
-#Gpu模式测试页面，目前正在测试中……暂时无法使用。
-import tensorflow as tf
-
-physical_devices = tf.config.list_physical_devices('GPU')
-if physical_devices:
-    print(f"TensorFlow is using the following GPU(s): {physical_devices}")
-else:
-    print("TensorFlow is using CPU. Please ensure that the correct GPU version is installed.")
